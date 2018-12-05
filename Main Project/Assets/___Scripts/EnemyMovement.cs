e98f46f5fbe9e5626eec9a103641dcb729546581
yrrag5 <G00335806@gmit.ie>
2018-12-05 20:41:43 +0000

    Updated stage select scene for navigation

--- Main Project/Assets/___Scripts/EnemyMovement.cs
+++ Main Project/Assets/___Scripts/EnemyMovement.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Setting enemy patrol movement 
+    [SerializeField]
+    private float enemyPatrolMov = 2f;
+
+    private Rigidbody2D rb;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Players  movement speed
+    private void FixedUpdate()
+    {
+        rb.velocity = Vector2.left * enemyPatrolMov;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
